2f3737bf26aa0f448b353f789e6dbefe04ef2eea
kasai <rikuriku918@gmail.com>
2022-03-18 10:13:46 +0900

    Initial Commit

---
+++ RKkadai1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RKkadai1
+//
+//  Created by kasai riku on 2022/03/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
