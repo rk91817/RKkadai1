98eb066af94ecb41587a12865396fbb7f067c4c5
kasai <rikuriku918@gmail.com>
2022-04-12 09:43:26 +0900

    swiftlintの修正

--- RKkadai1/ViewController.swift
+++ RKkadai1/ViewController.swift
@@ -26,10 +26,8 @@ class ViewController: UIViewController {
         let num3 = Double(textField3.text ?? "") ?? 0
         let num4 = Double(textField4.text ?? "") ?? 0
         let num5 = Double(textField5.text ?? "") ?? 0
-    
         label.text = calclation(number1: num1, number2: num2, number3: num3, number4: num4, number5: num5)
     }
-
     func calclation (number1: Double, number2: Double, number3: Double, number4: Double, number5: Double) -> String {
         let result = number1 + number2 + number3 + number4 + number5
         return result.description
